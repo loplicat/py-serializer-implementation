17ceb6f17f1ff07b060aa2b340a721397f53a6dc
Lana Chernenko <svitlaana.chernenko.s@gmail.com>
2024-04-25 12:13:09 +0100

    Implement the CarSerializer and functions

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
+import io
+from rest_framework.parsers import JSONParser
+from rest_framework.renderers import JSONRenderer
+
 from car.models import Car
+from car.serializers import CarSerializer
 
 
 def serialize_car_object(car: Car) -> bytes:
-    pass
+    serializer = CarSerializer(car)
+    json = JSONRenderer().render(serializer.data)
+    return json
 
 
 def deserialize_car_object(json: bytes) -> Car:
-    pass
+    stream = io.BytesIO(json)
+    data = JSONParser().parse(stream)
+    serializer = CarSerializer(data=data)
+    if serializer.is_valid():
+        return serializer.save()
+
+
+car2 = Car.objects.get(id=1)
+json2 = serialize_car_object(car2)
+print(json2)
+print(deserialize_car_object(json2))
